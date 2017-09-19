45a08ebf9e4393227d0b088a87b335b773d5ef23
designer <avadakkeddavra@gmail.com>
2017-09-19 11:39:24 +0300

    add xmlhttprequest functions (dont working)

--- background.js
+++ background.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
     $('#start').on('click',function(){
-        
+      var xmlHttp = null;
+	  xmlHttp = new XMLHttpRequest();
+
+	
+		xmlHttp.open("POST","https://ru.cs.deals/ajax/botsinventory",true)		
+		xmlHttp.setRequestHeader('accept', 'application/json, text/javascript, */*; q=0.01t');
+		xmlHttp.setRequestHeader('x-requested-with', 'XMLHttpRequest');
+		
+		
+		
+		xmlHttp.onreadystatechange = function() {
+		  if (xmlHttp.readyState == 4) {
+			 if(xmlHttp.status == 200) {
+			   console.log(xmlHttp.responseText);
+			}
+		  }
+		};
+		xmlHttp.send(null);
     })
 })
