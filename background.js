cb128e1720fa4c5543a2bd1d2cef9565f2f581e1
Alexey <avadakkeddavra@gmail.com>
2017-09-21 17:24:01 +0300

    add setInterval add avp_price checking almost

--- background.js
+++ background.js
@@ -1,41 +1,64 @@
-//$(document).ready(function(){
-//    $('#start').on('click',function(){
-//        $.ajax({
-//            url:"http://bot.poisk.zp.ua/",
-//            type:'POST',
-//            dataType:'json',
-//            success: function(response){
-//                console.log(response);
-//            }
-//            
-//        })
-//
-//    })
-//    $('#opskins').click(function(){
-//        $.ajax({
-//            url:"http://csgoback.net/api/extension?extension=csgoroll-bot&type=subscription&subType=loadPrice&service=opskins.com&updateTime=360&opskinsMinSales=0&opskinsOnSales=0",
-//            type:'GET',
-//            dataType: 'json',
-//            success: function(response){
-//                console.log(response);
-//            }
-//        })
-//    })
-//    
-//})
+function send_cs_deals_request()
+{
+    $.ajax({
+                url:"http://bot.poisk.zp.ua/",
+                type:'POST',
+                dataType:'json',
+                success: function(response){
+                    if(response['success'] == true)
+                    {
+                        var data = JSON.stringify(response)
+                       localStorage.setItem("cs_deals_json", data);
+                        
+                    }
+                  
+                }
+
+            });
+}
+function send_opskins_request(url,avg)
+{
+        $.ajax({
+                url:url,
+                type:'GET',
+                dataType: 'json',
+                success: function(response_opskins){
+                    var data = JSON.stringify(response_opskins)
+                    if(avg == true)
+                    {
+                        localStorage.setItem("opskins_avg_json", data);    
+                    }else{
+                        localStorage.setItem("opskins_json", data);
+                    }
+                    
+                    
+                }
+            })
+}
+/**
+*
+*    Restoring cs.deals array without wrong types of items and collects the items
+*
+*
+*
+*
+*
+*/
 function resort_cs_deals(json, options)
 {
     var array = json.response;
     var min_price = options.min_price;
+    var max_price = options.max_price;
     var index_for_delete = '';
-    var wrong_types = ['Music','Sticker','Graffiti','Souvenir','Capsule','Case', 'Legends','Gift','Key'];
+    var wrong_types = options.black_list.split(';');
+    //var wrong_types = ['Music','Sticker','Graffiti','Souvenir','Capsule','Case', 'Legends','Gift','Key'];
     var j = 0;
     var k = 0;
     var cs_deals_cleared = [];
     var quantity_items = [];
     $.each(array,function(i,elem){
         var price = elem.v;
-        if(elem.v <= min_price || $.inArray(elem.t,wrong_types) != -1 )
+        if(elem.v < min_price || elem.v > max_price || $.inArray(elem.t,wrong_types) != -1 )
         {
             index_for_delete += ''+i+','; 
         }else{
@@ -64,7 +87,16 @@ function resort_cs_deals(json, options)
 
     return cs_deals_cleared;
 }
+
+/**
+*
+*   Check has the name of item a some string with wrong type
+*   (FOR OPSKINS)
+*   
+*   @return boolean
+*/
 function strIndxOf(types, name){
+    
     
     for(var i = 0; i < types.length; i++)
     {
@@ -74,26 +106,60 @@ function strIndxOf(types, name){
             return 0;
         }
     }
-    return 1;
     
+    return 1;
 }
+/**
+*
+*   Restoring opskins array without wrong types of items
+*
+*
+*   @return array()
+*/
 function restore_opskins(json,options)
 {
     var array = json.priceList;
     var min_price = options.min_price;
-    var wrong_types = ['Music','Sticker','Graffiti','Souvenir','Container','Capsule','Case', 'Legends','Gift','Key'];
-    var opsiks = [];
+    var max_price = options.max_price;
+    var wrong_types = options.black_list.split(';');
+    //var wrong_types = ['Music','Sticker','Graffiti','Souvenir','Container','Capsule','Case', 'Legends','Gift','Key'];
+    var opskins = [];
     var j = 0;
+    console.log(wrong_types);
     for (var i in array)
     {   
-        if(strIndxOf(wrong_types,i) != 0 || array.price >= min_price)
+        if(wrong_types.length > 1)
         {
-            opsiks[i] = array[i];
+                
+            if(strIndxOf(wrong_types,i) != 0 )
+            {
+                if(array[i].price >= min_price && array[i].price <= max_price)
+                {
+                    
+                    opskins[i] = array[i];
+                }
+                
+            }
+        }else{
+            
+            if(array[i].price >= min_price && array[i].price <= max_price)
+            {     
+                  opskins[i] = array[i];      
+            }
         }
 
+
     }
-    return opsiks;
+    return opskins;
 }
+/**
+*
+*   Compares two arrays
+*
+*   
+*
+*   @return array() 
+*/
 function arrays_compare(opskins,cs_deals)
 {
     var result_opskins = [];
@@ -110,8 +176,6 @@ function arrays_compare(opskins,cs_deals)
             data.cs_deals = cs_deals[i];
             result_opskins[k] = data;
             k++;    
-        }else{
-            console.log(cs_deals[i]);
         }
        
     }
@@ -120,11 +184,22 @@ function arrays_compare(opskins,cs_deals)
     
     return result_opskins;
 }
+/**
+*
+*   Checking the price by formula
+*   
+*   @all_in_one -> array(opskins, cs_deals) -> 
+*   options -> object of users settings
+*
+*   @return the array() of items from percent interval
+*/
 function price_checking(all_in_one,options)
 {
     var response = [];
     var min_percent = options.min_percent;
     var max_percent = options.max_percent;
+    var final_collection = [];
+    var j = 0;
     //console.log(min_percent,max_percent)
     for(var i = 0; i < all_in_one.length; i++)
     {
@@ -133,56 +208,87 @@ function price_checking(all_in_one,options)
         var percent = (ops_price*95)/(cs_price)-100;
         if(percent > min_percent && percent < max_percent)
         {
-            console.log(all_in_one[i],percent);
+            if(options.compare == 'on')
+            {
+                var ops_avp_price = all_in_one[i].opskins.opsExtraPrice;
+                var avp_percent = (ops_avp_price*95)/(cs_price)-100;
+                if(avp_percent > min_percent && avp_percent < max_percent)
+                {
+                    final_collection[j] = all_in_one[i].cs_deals;
+                    j++;     
+                }                    
+            }else{
+                final_collection[j] = all_in_one[i].cs_deals;
+                j++; 
+            }
+
+
         }
     }
+    return final_collection;
+}
+
+function bot_start(options){
+    send_cs_deals_request();
+                  // formating the cs_deals json array
+                    var cs_deals_json = JSON.parse(localStorage.getItem("cs_deals_json"));
+                    var cs_deals_cleared = resort_cs_deals(cs_deals_json,options); 
+                    console.log(cs_deals_cleared);
+                    for(var i = 0; i < cs_deals_cleared.length; i++)
+                    {
+                        if(cs_deals_cleared[i].m == '★ Karambit | Doppler (Factory New)')
+                            {
+                                console.log(cs_deals_cleared[i]);
+                            }
+                    }
+            send_opskins_request(options.opskins_url,false);
+
+                  // fromating the opskins json array
+                  var opskins_json = JSON.parse(localStorage.getItem("opskins_json")); 
+                   console.log(opskins_json);
+                  var opskins_cleared_json = restore_opskins(opskins_json,options);
+                  console.log(opskins_cleared_json);
+
+                  // compearing two arrays
+                  var all_in_arr = arrays_compare(opskins_cleared_json,cs_deals_cleared);
+                 // first checkeng
+                 var final = price_checking(all_in_arr,options);
+                  console.log(final);
+}
+function bot_stop(init)
+{
+    clearInterval(init);
+    
 }
 chrome.runtime.onMessage.addListener(
+    
   function(request, sender, sendResponse) {
     var options = request.options; // данные о сайте
-      console.log(options)
+    var stop = request.options;
+    console.log(options.stop != 1)
       
-            $.ajax({
-                url:"http://bot.poisk.zp.ua/",
-                type:'POST',
-                dataType:'json',
-                success: function(response){
-                    if(response['success'] == true)
-                    {
-                        var data = JSON.stringify(response)
-                       localStorage.setItem("cs_deals_json", data);
-                        
-                    }
-                  
-                }
+      if(options.stop != 1)
+        {
+          var init = setInterval(function(){
+                
+                bot_start(options);
+                //console.log('bot_start');
+            },options.rate);
+            
+            localStorage.setItem("interval", init);
+        }else{
+         var int = localStorage.getItem("interval")
+                clearInterval(int);  
+            console.log('bot_stop');
+        }
+            [8,"nc86m9snsn4vkcmupf0jh7eg1v",{"type":"offchange","id":"2516947375","msg":"Trade completed","class":"success","accepted":true,"pricein":0}]
 
-            });
+           
+        [8,"nc86m9snsn4vkcmupf0jh7eg1v",{"type":"offers","msg":[{"id":"2516947375","conf":0,"btcamount":"0.00000000","usdamount":"0.00","btcaddress":"","u":0,"b":1}]}]
       
       
-      
-    var cs_deals_json = JSON.parse(localStorage.getItem("cs_deals_json"));
-        //console.log(cs_deals_json);
-      var cs_deals_cleared = resort_cs_deals(cs_deals_json,options);
-      
-      //console.log(cs_deals_cleared);
-
-            $.ajax({
-                url:options.opskins_url,
-                type:'GET',
-                dataType: 'json',
-                success: function(response_opskins){
-    
-                    var data = JSON.stringify(response_opskins)
-                    localStorage.setItem("opskins_json", data);
-                }
-            })
-      var opskins_json = JSON.parse(localStorage.getItem("opskins_json"));
-     // console.log(opskins_json["★ Karambit | Case Hardened (Well-Worn)"]);
-      var opskins_cleared_json = restore_opskins(opskins_json,options);
-      //console.log(opskins_cleared_json);
-      //console.log('compare');
-      var all_in_arr = arrays_compare(opskins_cleared_json,cs_deals_cleared);
-     // console.log(all_in_arr);
-      price_checking(all_in_arr,options);
+    [8,"nc86m9snsn4vkcmupf0jh7eg1v",{"type":"offers","msg":[{"id":"2516947375","conf":1,"btcamount":"0.00000000","usdamount":"0.00","btcaddress":"","u":0,"b":1}]}]
+        
+  
 
 });
--- common.js
+++ common.js
@@ -2,6 +2,39 @@
 function onClick(options) { // окно теряет фокус
 	chrome.runtime.sendMessage({options:options}); // отправка сообщения на background.js
 }
+function stop(opt)
+{
+    chrome.runtime.sendMessage({options:opt}); // отправка сообщения на background.js
+}
+function validate(data)
+{
+   if( $('.success').length >=  7)
+   {
+       return 1;
+   }
+}
+$(document).ready(function(){
+    $('#percentage_form input').not("[type='checkbox']").unbind().on('blur',function(){
+      var type = $(this).attr('type');
+      var value = $(this).val();
+      
+      switch (type)
+      {
+          case 'number':
+            if($.isNumeric(value) && value.length > 0)
+            {
+                $(this).addClass('success');
+                $(this).removeClass('error');
+                $(this).parent().find('span').remove();
+            }else{
+                $(this).addClass('error');
+                $(this).parent().append('<span style="color:red">Это поле обязательно для заполнения</span>');
+            }
+          break;
+      }
+    })
+})
+
 $(document).ready(function(){
     $('#toggle_exists').on('click',function(e){  
        if($(this).hasClass('active_check'))
@@ -17,22 +50,44 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $('#start').on('click',function(e){
-        
             e.preventDefault();
+        if($(this).hasClass('start'))
+        {
+                
+            $(this).addClass('stop');
+            $(this).removeClass('start');
+            $(this).text('Stop');
             var data = $('#percentage_form').serializeArray();
-            console.log(data);
 
-            var quatity_per_week = data[0].value;
-            var options = new Object;			
-            jQuery.each(data, function(i, item){
-                options[item.name] = item.value;
-            
-			});
-					options['opskins_url'] = "http://csgoback.net/api/extension?extension=csgoroll-bot&type=subscription&subType=loadPrice&service=opskins.com&updateTime=360&opskinsMinSales="+quatity_per_week+"&opskinsOnSales=0";
-					
-					console.log(options);
+                var quatity_per_week = data[0].value;
+                var quantity_sales = data[1].value; 
+                var options = new Object;			
+                jQuery.each(data, function(i, item){
+                    options[item.name] = item.value;
 
+                });
+
+               options['opskins_url'] = "http://csgoback.net/api/extension?extension=csgoroll-bot&type=subscription&subType=loadPrice&service=opskins.com&updateTime=360&opskinsMinSales="+quatity_per_week+"&opskinsOnSales="+quantity_sales+"";
+        
+               options['opskins_avg_url'] = "http://csgoback.net/api/extension?extension=csgoroll-bot&type=subscription&subType=loadPrice&service=opskins.com_avg&updateTime=360&opskinsMinSales="+quatity_per_week+"&opskinsOnSales="+quantity_sales+"";
+
+              var response = validate(options);
+        
+                if(response == 1)
+                {
                     onClick(options);
+                }
+        }else{
+            console.log('func');
+            var st = 1;
+            var options = new Object;			
+            options.rate = 0;
+            options.stop = 1;
+            stop(options);
+            $(this).removeClass('stop');
+            $(this).addClass('start');
+            $(this).text('Start')
+        }
 
     })
 })
